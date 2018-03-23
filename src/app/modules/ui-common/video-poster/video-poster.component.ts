bb89afb5a2bb8758b21317734c7238b6d47099eb
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2018-03-23 19:35:57 -0300

    feat(cinemagraph-poster-watermaked): Adding functionality for generate posters that are watermaked

--- src/app/modules/ui-common/video-poster/video-poster.component.ts
+++ src/app/modules/ui-common/video-poster/video-poster.component.ts
@@ -6,22 +6,51 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./video-poster.component.scss']
 })
 export class VideoPosterComponent implements OnInit {
-  private hostUrl = 'https://s3-eu-west-1.amazonaws.com/video.gallereplay.com/portfolio/clients';
-  private baseUrl = `${this.hostUrl}/jaegermeister/Cinemagraph_plain/1920x1080`;
-  
-  @Input()
-  public videoUrls = {
-    poster: `${this.baseUrl}/cinemagraph.jpg`,
-    mp4: `${this.baseUrl}/cinemagraph.mp4`,
-    webm: `${this.baseUrl}/cinemagraph.webm`,
-  }
+  /* Example URL
+    https://s3-eu-west-1.amazonaws.com/video.gallereplay.com/artistarea
+    /Preparing Pumpkin Dish_91aab030-43b3-4b1f-805e-2e1fe37a36d6/Cinemagraph_watermark_purchasepage
+    /2_large_files/cinemagraph.jpg
+  */
+  public videoUrls;
   @Input() public title = 'Custom Cinemagraph Productions';
   @Input() public subtitle = 'Exclusive Content for Businesses';
   @Input() public showAuthorInfo = true;
+  @Input() public isWatermarked = false;
+
+  private baseUrl;
+  private itemIdentifier;
+  private accessFolder;
+  private cinemagraphSize;
+  private cinemagraphBaseUrl;
 
   constructor() { }
 
   ngOnInit() {
-
+    this.baseUrl = {
+      artist: 'https://s3-eu-west-1.amazonaws.com/video.gallereplay.com/artistarea',
+      client: 'https://s3-eu-west-1.amazonaws.com/video.gallereplay.com/portfolio/clients'
+    };
+    this.itemIdentifier = `Preparing Pumpkin Dish_91aab030-43b3-4b1f-805e-2e1fe37a36d6`;
+    this.accessFolder = {
+      public: 'Cinemagraph_plain',
+      watermark: 'Cinemagraph_watermark_purchasepage',
+    };
+    this.cinemagraphSize = {
+      thumbnail: '',
+      medium: '2_medium-sized_files',
+      watermark_large: '2_large_files',
+      large: '3_large_files'
+    };
+    this.cinemagraphBaseUrl = this.urlBuilder();
+    this.videoUrls = {
+      poster: `${this.cinemagraphBaseUrl}/cinemagraph.jpg`,
+      mp4: `${this.cinemagraphBaseUrl}/cinemagraph.mp4`,
+      webm: `${this.cinemagraphBaseUrl}/cinemagraph.webm`,
+    };
+  }
+  urlBuilder() {
+    const urlAccess = this.isWatermarked ? this.accessFolder.watermark : this.accessFolder.public;
+    const size = this.isWatermarked ? this.cinemagraphSize.watermark_large : this.cinemagraphSize.large;
+    return `${this.baseUrl.artist}/${this.itemIdentifier}/${urlAccess}/${size}`;
   }
 }
--- stories/index.ts
+++ stories/index.ts
@@ -8,13 +8,26 @@ storiesOf('Video Poster component', module)
   .add('Video Poster with author info, custom title and subtitle', () => ({
     component: VideoPosterComponent,
     props: {
-        title: "Cinemagraph With Custom title",
-        subtitle: "This is a custom subtitle!"
+      title: "Cinemagraph With Custom title",
+      subtitle: "This is a custom subtitle!"
     }
   }))
   .add('Video Poster without author info', () => ({
     component: VideoPosterComponent,
     props: {
+      showAuthorInfo: false
+    }
+  }))
+  .add('Video Poster Watermarked with author info, custom title and subtitle', () => ({
+    component: VideoPosterComponent,
+    props: {
+      isWatermarked: true
+    }
+  }))
+  .add('Video Poster Watermarked without author info', () => ({
+    component: VideoPosterComponent,
+    props: {
+      isWatermarked: true,
       showAuthorInfo: false
     }
   }));
