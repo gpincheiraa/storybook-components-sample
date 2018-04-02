e3de3de5faa8a1bbeab44468ada147c7ff8e20fe
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2018-04-02 11:18:29 -0300

    adding app gmap component to app main component for demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AgmCoreModule } from '@agm/core';
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction'; 
 import { UICommonModule } from './modules/ui-common/ui-common.module';
 import { GoogleMapsModule } from './modules/google-maps/google-maps.module';
 import { GmapComponent } from '../../src/app/modules/google-maps/gmap/gmap.component';
+import { GMapsService } from '../../src/app/modules/google-maps/services/google-maps.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,10 +15,17 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         UICommonModule,
-        AgmCoreModule,
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyDuJqxX0rgvwpmQ4Z4dxJjxQKzBSpIq7OU',
+            libraries: [ 'places' ]
+        }),
+        AgmDirectionModule,
         GoogleMapsModule
     ],
-    providers: [],
+    providers: [
+        GoogleMapsAPIWrapper,
+        GMapsService
+    ],
     bootstrap: [
         AppComponent
     ]
